32d4ab7d82b376a4b6012a9a43b7390b2732a53f
anv <anv@woori.ru>
2023-02-06 17:55:04 +0300

    add session, flash

--- main.py
+++ main.py
@@ -1,14 +1,42 @@
 from flask import request
 from flask import make_response
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, session, redirect,flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
+
+class NameForm(FlaskForm):
+    name = StringField("INSERT YOUR NAME?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 
 @app.route('/')
 def index():
     data = "START PAGE"
-    return render_template("index.html", data= data), 200
+    return render_template("index.html", data=data), 200
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    info = ''
+    name = None
+    data = "START PAGE"
+    form = NameForm()
+
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash("You changed your name!")
+
+        session['name'] = form.name.data
+        return redirect(url_for('login'))
+
+
+    return render_template("login.html", data=data, form=form, name=session.get('name'),info=info), 200
+
 
 @app.route('/about/')
 def about():
